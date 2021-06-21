251db294f6520c85b882c1060a41464971b62c63
Paras Mendiratta <code.snooker@gmail.com>
2021-06-21 23:00:45 +0530

    completed react draggable poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import RGL, { WidthProvider } from 'react-grid-layout'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ReactGridLayout = WidthProvider(RGL)
+
+class DemoLayout extends React.PureComponent {
+  static defaultProps = {
+    isDraggable: true,
+    isResizable: true,
+    items: 5,
+    rowHeight: 30,
+    preventCollision: false,
+    compactType: null,
+    cols: 12,
+  }
+
+  render() {
+    return (
+      <ReactGridLayout {...this.props}>
+        <div key='1' data-grid={{ x: 0, y: 0, w: 3, h: 3, static: false }}>
+          <span>MOVE</span>
+        </div>
+        <div key='2' data-grid={{ x: 0, y: 0, w: 3, h: 3, static: false }}>
+          <span>MOVE</span>
+        </div>
+        <div key='3' data-grid={{ x: 0, y: 0, w: 3, h: 3, static: true }}>
+          <span>STATIC</span>
+        </div>
+      </ReactGridLayout>
+    )
+  }
 }
 
-export default App;
+export default DemoLayout
